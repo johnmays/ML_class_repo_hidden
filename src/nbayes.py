1ae916d0e468f02345d0ea470422d7f92d628941
James McHargue <jpm221@case.edu>
2022-10-19 22:56:46 -0400

    First draft of nbayes.

--- src/nbayes.py
+++ src/nbayes.py
@@ -1,26 +1,134 @@
 import argparse
+import os.path
 import numpy as np
 from typing import List
+
+import util
 
 from sting.classifier import Classifier
 from sting.data import Feature, FeatureType, parse_c45
 
 class NaiveBayes(Classifier):
-    def __init__(self, schema: List[Feature], numbins: int, m: int) -> None:
+    def __init__(self, schema: List[Feature], num_bins: int, m: int) -> None:
         self.schema = schema
-        self.num_bins = numbins
+        self.num_bins = num_bins
         self.ess = m
-        pass
+
+        self.prior = 0
+        self.model = []
+        # The ith index of self.model contains conditional probabilities
+        # associated with the ith attribute of schema.
+        for feature in schema:
+            if feature.ftype == FeatureType.NOMINAL:
+                self.model.append(np.zeros((2, len(feature.values))))
+            else:
+                self.model.append(np.zeros((2, self.num_bins)))
+        self.ranges = []
+
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        pass
+        num_ones = sum(y)
+        num_zeros = len(y) - num_ones
+        self.prior = num_ones / len(y)
+
+        # Calculate smoothed conditional probabilities
+        for index in range(len(self.schema)):
+            feature = self.schema[index]
+            parameters = self.model[index]
+            examples = np.array(X[:, index])
+            if feature.ftype == FeatureType.CONTINUOUS:
+                V = self.num_bins
+                # Record ranges of continuous attributes
+                minimum = np.min(examples)
+                maximum = np.max(examples)
+                bin_width = (maximum - minimum) / self.num_bins
+                self.ranges.append((minimum, maximum, bin_width))
+                # Vectorize conversion to bin indices
+                binned = np.floor((examples - minimum) / bin_width)
+            else:
+                V = len(feature.values)
+                self.ranges.append(None)
+                # Convert examples to bin indices
+                binned = examples - 1
+            # Gather counts
+            for j in range(len(binned)):
+                bin = min(binned[j], V - 1)
+                parameters[int(y[j])][int(bin)] += 1
+            # Smoothing
+            for b in range(V):
+                parameters[0][b] = (parameters[0][b] + (self.ess / V)) / (num_zeros + self.ess)
+                parameters[1][b] = (parameters[1][b] + (self.ess / V)) / (num_ones + self.ess)
+    
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        pass
+        """
+        This method uses the Naive Bayes classifier to label a set of examples, X.
+
+        Args:
+            X: Testing data of shape (n_examples, n_features).
+
+        Returns: Predictions of shape (n_examples,), either 0 or 1
+        """
+        y = []
+        for example in X:
+            # 'likelihood' tracks the probability of X, given 0 and 1 respectively
+            likelihood = [1, 1]
+            for index in range(len(self.schema)):
+                feature = self.schema[index]
+                parameters = self.model[index]
+                if feature.ftype == FeatureType.CONTINUOUS:
+                    r = self.ranges[index]
+                    bin = int(min((example[index] - r[0]) / r[2], self.num_bins - 1))
+                    likelihood[0] *= parameters[0][bin]
+                    likelihood[1] *= parameters[1][bin]
+                else:
+                    bin = int(example[index] - 1)
+                    likelihood[0] *= parameters[0][bin]
+                    likelihood[1] *= parameters[1][bin]
+
+            y.append((likelihood[1] >= likelihood[0]) * 1)
+
+        return y
 
 
-def evaluate_and_print_metrics(nbayes: NaiveBayes, X: np.ndarray, y: np.ndarray):
-    pass
+def evaluate_and_print_metrics(nb: NaiveBayes, X: np.ndarray, y: np.ndarray):
+    """
+    Print information about the performance of the naive Bayes classifier on testing data.
+
+    Args:
+        nb: the naive Bayes classifier to evaluate.
+        X: An array of examples to use for evaluation.
+        y: An array of class labels associated with the examples in X.
+    """
+
+    y_hat = nb.predict(X)
+    acc = util.accuracy(y, y_hat)
+    #precision = util.precision(y, y_hat)
+    #recall = util.recall(y, y_hat)
+    #auc = util.auc(y, y_hat)
+    print("\n***********\n* RESULTS *\n***********")
+    print(f'Accuracy:{acc:.2f}')
+    #print('Precision:', dtree.size)
+    #print('Recall:', dtree.depth)
+    #print('AUR:', dtree.schema[dtree.root.attribute_index].name, '\n')
+
+
+def nbayes(data_path: str, num_bins: int, m: int, use_cross_validation: bool = True):
+    # last entry in the data_path is the file base (name of the dataset)
+    path = os.path.expanduser(data_path).split(os.sep)
+    file_base = path[-1]  # -1 accesses the last entry of an iterable in Python
+    root_dir = os.sep.join(path[:-1])
+    schema, X, y = parse_c45(file_base, root_dir)
+
+    if use_cross_validation:
+        datasets = util.cv_split(X, y, folds=5, stratified=True)
+    else:
+        datasets = ((X, y, X, y),)
+    
+    for X_train, y_train, X_test, y_test in datasets:
+        classifier = NaiveBayes(schema, num_bins, m)
+        classifier.fit(X_train, y_train)
+        evaluate_and_print_metrics(classifier, X_test, y_test)
 
 
 if __name__ == "__main__":
@@ -34,3 +142,11 @@ if __name__ == "__main__":
                         help='Disables cross validation and trains on the full dataset.')
     parser.set_defaults(cv=True, gain_ratio=False)
     args = parser.parse_args()
+
+    data_path = os.path.expanduser(args.path)
+    num_bins = args.numbins
+    m = args.m
+    use_cross_validation = args.cv
+
+    nbayes(data_path, num_bins, m, use_cross_validation)
+
