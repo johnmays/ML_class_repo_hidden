82f4a18ca7782e6eddf694e692cfcf64b779065d
Zaichuan You <zxy456@case.edu>
2022-10-27 02:35:52 -0400

    logreg update and fit implemented in Test.py

--- src/Test.py
+++ src/Test.py
@@ -3,10 +3,11 @@ from sting.data import Feature, FeatureType, parse_c45
 import os
 from logreg import LogReg
 import numpy as np
+import util
 
 
 # last entry in the data_path is the file base (name of the dataset)
-path = os.path.expanduser("C:\\Users\\21995\\Desktop\\Computer Science\\CSDS 440\\Programming\\440data\\voting").split(os.sep)
+path = os.path.expanduser("C:\\Users\\21995\\Desktop\\Computer Science\\CSDS 440\\Programming\\440data\\volcanoes").split(os.sep)
 file_base = path[-1]  # -1 accesses the last entry of an iterable in Python
 root_dir = os.sep.join(path[:-1])
 schema, X, y = parse_c45(file_base, root_dir)
@@ -14,9 +15,24 @@ schema, X, y = parse_c45(file_base, root_dir)
     if a.ftype == FeatureType.NOMINAL:
         print(a.nominal_values)
 print(X[0])"""
-log = LogReg(0.1)
-print(len(X[0]))
-print(np.shape(X[0]))
-W = np.random.randn(11,)
+np.random.seed(300)
+
+W = np.random.randn(len(X[0]),)+1
 B = np.random.randn()
-print(log.logistic_reg(W, X[0], B))
+log = LogReg(0.01, W, B, 0.1)
+costs = []
+for i in range(0, 100):
+    costs = np.append(costs, log.cost(X[0]))
+    for example in range(0, len(y)):
+        gradient_w = log.gradient_w(X[example])
+        gradient_b = log.gradient_b(X[example])
+        #log.W = log.W-(log.logistic_reg(X[example])-y[example])*log.rate*gradient_w
+        #log.B = log.B-(log.logistic_reg(X[example])-y[example])*log.rate*gradient_b
+        log.W = log.W-log.rate*gradient_w
+        log.B = log.B-log.rate*gradient_b
+
+#print(log.W)
+#print(log.B)
+print(costs)
+
+print(util.accuracy(log.predict(X), y))
--- src/logreg.py
+++ src/logreg.py
@@ -1,32 +1,57 @@
 import argparse
 import numpy as np
 import math
+from numpy import linalg as LA
 
 from sting.classifier import Classifier
 from sting.data import Feature, FeatureType, parse_c45
 
+
 class LogReg(Classifier):
-    def __init__(self, lamb) -> None:
+    def __init__(self, lamb, W, B, rate) -> None:
         self.lamb = lamb
+        self.W = W
+        self.B = B
+        self.rate = rate
         pass
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         pass
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        pass
+        W = self.W
+        B = self.B
+        prediction = np.sum(W * X, axis=1)+B
+        for a in range(0, len(prediction)):
+            if prediction[a] > 0:
+                prediction[a] = 1
+            else:
+                prediction[a] = 0
+        return prediction
     
-    def gradient_w(self, W: np.ndarray, X: np.ndarray, B: np.ndarray):
-        #The partial derivative of the function respect to W
-        return X/(1+math.e**(-W*X+B))+self.lamb*W
+    def gradient_w(self, X: np.ndarray):
+        W = self.W
+        B = self.B
+        lamb = self.lamb
+        #The partial derivative of the function respect to W 
+        return X/(1+np.exp(-np.sum(W*X)+B))+lamb*W
     
-    def gradient_b(self, W: np.ndarray, X: np.ndarray, B: np.ndarray):
+    def gradient_b(self, X: np.ndarray):
+        W = self.W
+        B = self.B
         #The partial derivative of the function respect to B
-        return -1/(1+math.e**(-W*X+B))
+        return -1/(1+np.exp(-np.sum(W*X)+B))
     
-    def logistic_reg(self, W: np.ndarray, X: np.ndarray, B: np.ndarray):
+    def logistic_reg(self, X: np.ndarray):
+        W = self.W
+        B = self.B
         #The logistic regression equation
-        return 1/(1+math.e**(-(np.sum(W*X)+B)))
+        return 1/(1+np.exp(-(np.sum(W*X)+B)))
+    
+    def cost(self, X):
+        W = self.W
+        B = self.B
+        return -np.log(1-(1/(1+np.exp(-np.sum(W*X)+B)))) + self.lamb/2*LA.norm(W**2)
 
 
 def evaluate_and_print_metrics(logreg: LogReg, X: np.ndarray, y: np.ndarray):
