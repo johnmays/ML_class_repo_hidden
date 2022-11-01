8e5a3ae7cae94baf387702c59d61e80de5382019
Zaichuan You <zxy456@case.edu>
2022-11-01 19:35:44 -0400

    Finalize logreg_rethink

--- src/Test.py
+++ src/Test.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LogisticRegression
 
 
 # last entry in the data_path is the file base (name of the dataset)
-path = os.path.expanduser("C:\\Users\\21995\\Desktop\\Computer Science\\CSDS 440\\Programming\\440data\\volcanoes").split(os.sep)
+path = os.path.expanduser("C:\\Users\\21995\\Desktop\\Computer Science\\CSDS 440\\Programming\\440data\\spam").split(os.sep)
 file_base = path[-1]  # -1 accesses the last entry of an iterable in Python
 root_dir = os.sep.join(path[:-1])
 schema, X, y = parse_c45(file_base, root_dir)
@@ -20,21 +20,22 @@ np.random.seed(300)
 
 W = np.random.randn(len(X[0]),)+1
 B = np.random.randn()
-log = LogReg(0.1, W, B, 0.1)
+log = LogReg(0.1, W, B, 0.01)
 print(W)
 print(B)
-costs = []
-for i in range(0, 200):
+"""costs = []
+for i in range(0, 100):
     costs.append(log.cost(X, y))
-    for example in range(0, len(y)):
-        X_temp = X[example].copy()
-        gradient_w = log.gradient_w(X[example], y[example])
-        gradient_b = log.gradient_b(X[example], y[example])
-        #print(f"old W:{log.W}, old B:{log.B}")
-        #print(f"gradient W:{gradient_w}, gradient B:{gradient_b}")
-        log.W = log.W - (log.rate*gradient_w)
-        log.B = log.B - (log.rate*gradient_b)
-        #print(f"new W:{log.W}, new B:{log.B}")
+    #X_temp = X[example].copy()
+    gradient_w = log.dw(X, y)
+    gradient_b = log.db(X, y)
+    #print(f"old W:{log.W}, old B:{log.B}")
+    #print(f"gradient W:{gradient_w}, gradient B:{gradient_b}")
+    log.W = log.W - (log.rate*gradient_w)
+    #log.B = log.B - (log.rate*gradient_b)
+    #print(f"new W:{log.W}, new B:{log.B}")"""
+
+costs = log.fit(X, y, 20)
 
 print(log.W)
 print(log.B)
